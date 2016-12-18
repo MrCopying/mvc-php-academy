086b3b775f488a914de3562fb362671d7ff7a1dd
Mr.Copying (Kozlov Andrew) <mr.copying@gmail.com>
2016-12-18 17:58:44 +0200

    lesson 4 lib-class __autoload config router

--- webroot/index.php
+++ webroot/index.php
@@ -1,7 +1,9 @@
 <?php
-echo 'saf';
+
 define('DS',DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
-$uri = $_SERVER['REQUEST_URI'];
 
-print_r($uri);
+require_once(ROOT.DS.'lib'.DS.'init.php');
+
+$router = new Router($_SERVER['REQUEST_URI']);
+
